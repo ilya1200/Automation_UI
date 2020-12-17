e76c12204a19c14304f9e998798ad7cf62f09f22
ilya <ilya1200@gmail.com>
2020-12-17 23:38:47 +0200

    add allure screenshots

--- tests/Alerts/test_alert_box.py
+++ tests/Alerts/test_alert_box.py
@@ -1,3 +1,4 @@
+import allure
 from assertpy import assert_that
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
@@ -10,21 +11,35 @@ class TestAlerts:
     def setup_class(self):
         self.selenium_infra = SeleniumInfra()
 
+    @allure.title("test_alert_box")
     def test_alerts(self):
-        self.selenium_infra.get(self.ALERTS_URL)
-        self.selenium_infra.find_element_by(By.XPATH, "//button[contains(text(), 'alert box')]").click()
-        alert = self.selenium_infra.driver.switch_to.alert
+        with allure.step(f"When user goes Register Page"):
+            self.selenium_infra.get(self.ALERTS_URL)
+            self.selenium_infra.screenshot_for_allure()
 
-        assert_that(alert.text) .is_equal_to("I am an alert box!")
-        alert.accept()
+        with allure.step(f"And click on the display alert button"):
+            self.selenium_infra.find_element_by(By.XPATH, "//button[contains(text(), 'alert box')]").click()
 
-        try:
-            alert = self.selenium_infra.driver.switch_to.alert
-            is_alert = True
-        except NoAlertPresentException as e:
-            is_alert = False
-        assert_that(is_alert, "Expected the alert to disappear").is_false()
+        with allure.step(f"Then an alert should appear"):
+            self.alert = self.selenium_infra.driver.switch_to.alert
+
+        with allure.step(f"And should contain text - 'I am an alert box!' "):
+            assert_that(self.alert.text) .is_equal_to("I am an alert box!")
+
+        with allure.step(f"When user click ok"):
+            self.alert.accept()
+            self.selenium_infra.screenshot_for_allure()
+
+        with allure.step(f"Then the alert should disappear"):
+            try:
+                self.alert = self.selenium_infra.driver.switch_to.alert
+                is_alert = True
+            except NoAlertPresentException as e:
+                is_alert = False
+            assert_that(is_alert, "Expected the alert to disappear").is_false()
+            self.selenium_infra.screenshot_for_allure()
 
     def teardown_class(self):
+        self.selenium_infra.screenshot_for_allure()
         self.selenium_infra.close()
         self.selenium_infra.quit()
