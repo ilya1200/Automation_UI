091deeb0826d02030386d8e892532ec8c175211d
ilya <ilya1200@gmail.com>
2020-12-18 21:33:48 +0200

    add config

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+TIME_TO_WAIT = 20
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CHROME_DRIVER_PATH = str(Path(f'{ROOT_DIR}//drivers/chromedriver.exe'))
+
+# Page URLs
+REGISTER_PAGE_URL = "http://demo.automationtesting.in/Register.html"
+WEB_TABLE_PAGE_URL = 'http://demo.automationtesting.in/WebTable.html'
+FRAMES_PAGE_URL = "http://demo.automationtesting.in/Frames.html"
+ALERTS_PAGE_URL = "http://demo.automationtesting.in/Alerts.html"
--- pom/pages/InitPages/InitPages.py
+++ pom/pages/InitPages/InitPages.py
@@ -1,3 +1,4 @@
+from config import REGISTER_PAGE_URL, WEB_TABLE_PAGE_URL
 from infrastructure.SeleniumInfra import SeleniumInfra
 from pom.components.InitComponents.InitComponents import InitComponents
 from pom.pages.RegisterPage.RegisterPage import RegisterPage
@@ -7,9 +8,6 @@ from pom.pages.WebTablePage.WebTablePageLocators import WebTablePageLocators
 
 
 class InitPages:
-    REGISTER_URL = "http://demo.automationtesting.in/Register.html"
-    WEB_TABLE_URL = 'http://demo.automationtesting.in/WebTable.html'
-
     def __init__(self, selenium_infra: SeleniumInfra, components: InitComponents):
-        self.register_page = RegisterPage(selenium_infra, RegisterPageLocators(), self.REGISTER_URL, components.register_form)
-        self.web_table_page = WebTablePage(selenium_infra, WebTablePageLocators(), self.WEB_TABLE_URL)
+        self.register_page = RegisterPage(selenium_infra, RegisterPageLocators(), REGISTER_PAGE_URL, components.register_form)
+        self.web_table_page = WebTablePage(selenium_infra, WebTablePageLocators(), WEB_TABLE_PAGE_URL)
