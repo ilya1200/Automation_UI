f0054fd75485e8e0719a5bee562f2219664a8fdb
ilya <ilya1200@gmail.com>
2020-12-18 21:35:40 +0200

    add url

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CHROME_DRIVER_PATH = str(Path(f'{ROOT_DIR}//drivers/chromedriver.exe'))
 
 # Page URLs
+AUTOMATION_TESTING_PAGE_URL = "http://demo.automationtesting.in/"
 REGISTER_PAGE_URL = "http://demo.automationtesting.in/Register.html"
 WEB_TABLE_PAGE_URL = 'http://demo.automationtesting.in/WebTable.html'
 FRAMES_PAGE_URL = "http://demo.automationtesting.in/Frames.html"
