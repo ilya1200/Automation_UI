65127d8a96c1f0394ba8c39fbe47a08b8d74310d
ilya1200 <ilya1200@gmail.com>
2022-01-14 19:59:36 +0200

    add to_dict and str

--- pom/components/CareerItem/CareerItem.py
+++ pom/components/CareerItem/CareerItem.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Any
 from selenium.webdriver.remote.webelement import WebElement
 from infrastructure.SeleniumInfra import SeleniumInfra
 from pom.components.CareerItem.CareerItemLocators import CareerItemLocators
@@ -6,7 +6,8 @@ from pom.components.Component.Component import Component
 
 
 class CareerItem(Component):
-    def __init__(self, selenium_infra: SeleniumInfra, locators: CareerItemLocators, element: WebElement = None, **kwargs):
+    def __init__(self, selenium_infra: SeleniumInfra, locators: CareerItemLocators, element: WebElement = None,
+                 **kwargs):
         super().__init__(selenium_infra, locators, **kwargs)
         self.locators: CareerItemLocators = locators
         self._element: WebElement = element
@@ -45,7 +46,19 @@ class CareerItem(Component):
     def tags(self) -> List[str]:
         tags_elements: List[WebElement] = self.selenium_infra.find_element_list_by(*self.locators.tags,
                                                                                    from_element=self.element)
-        return list(map(lambda tags_element: tags_element.text(), tags_elements))
+        return list(map(lambda tags_element: tags_element.text, tags_elements))
 
     def explore_position(self):
         self.selenium_infra.click_element(*self.locators.link, from_element=self.element)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "link": self.link,
+            "title": self.title,
+            "subtitle": self.subtitle,
+            "city": self.city,
+            "tags": self.tags
+        }
+
+    def __str__(self):
+        return str(self.to_dict())
