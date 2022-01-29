b3f6f4cb30eb8f610d0b4b7793a1c029e7f9476c
ilya1200 <ilya1200@gmail.com>
2022-01-29 11:05:53 +0200

    Update URLs

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CHROME_DRIVER_PATH = str(Path(f'{ROOT_DIR}//drivers/chromedriver.exe'))
 
 # Page URLs
-ISRONSOURCE_URL = "https://company.is.com/"
-CAREERS_ISRAEL_PAGE_URL = "https://company.is.com/careers-israel/"
+TECHDEMO_PAGE_URL = "https://techdemotbaseo.kinsta.cloud/"
+TECHDEMO_ABOUT_PAGE_URL = "https://techdemotbaseo.kinsta.cloud/about-us/"
